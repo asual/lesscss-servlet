1009b2c5bd894af964c5619e06c44f5b9efd12f5
asual <rostislav@asual.com>
2009-10-07 19:44:30 +0300

    Adds YUI Compressor support.

--- src/main/java/com/asual/lesscss/LessServlet.java
+++ src/main/java/com/asual/lesscss/LessServlet.java
@@ -16,9 +16,14 @@
 
 package com.asual.lesscss;
 
+import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.Reader;
+import java.io.Writer;
 import java.net.URL;
 import java.util.Date;
 import java.util.HashMap;
@@ -34,6 +39,11 @@ import javax.servlet.http.HttpServletResponse;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import org.mozilla.javascript.ErrorReporter;
+import org.mozilla.javascript.EvaluatorException;
+
+import com.yahoo.platform.yui.compressor.CssCompressor;
+import com.yahoo.platform.yui.compressor.JavaScriptCompressor;
 
 /**
  * @author Rostislav Hristov
@@ -47,18 +57,6 @@ public class LessServlet extends HttpServlet {
     private int milliseconds = 1000;
     private LessEngine engine;
     private LessCache cache;
-    
-    private Map<String, String> mimeTypes = new HashMap<String, String>();
-    
-    {
-        mimeTypes.put(".css", "text/css");
-        mimeTypes.put(".gif", "image/gif");
-        mimeTypes.put(".ico", "image/vnd.microsoft.icon");
-        mimeTypes.put(".jpeg", "image/jpeg");
-        mimeTypes.put(".jpg", "image/jpeg");
-        mimeTypes.put(".js", "text/javascript");
-        mimeTypes.put(".png", "image/png");
-    }
     
     @PostConstruct
     public void init() {
@@ -110,21 +108,74 @@ public class LessServlet extends HttpServlet {
                 return;
             }
             
+            Map<String, String> mimeTypes = new HashMap<String, String>();
+            
+            {
+                mimeTypes.put("css", "text/css");
+                mimeTypes.put("gif", "image/gif");
+                mimeTypes.put("ico", "image/vnd.microsoft.icon");
+                mimeTypes.put("jpeg", "image/jpeg");
+                mimeTypes.put("jpg", "image/jpeg");
+                mimeTypes.put("js", "text/javascript");
+                mimeTypes.put("png", "image/png");
+            }
+            
             String path = url != null ? url.getPath() : file.getAbsolutePath();
-            String extension = path.substring(path.lastIndexOf("."));
+            String extension = path.substring(path.lastIndexOf(".") + 1);
             String mimeType = (mimeTypes.containsKey(extension)) ? mimeTypes.get(extension) : getServletContext().getMimeType(path);
 
             byte[] content = null;
                         
             try {
-                if ("text/css".equals(mimeType)) {
-                    content = (url != null ? engine.compile(url) : engine.compile(file)).getBytes(engine.getCharset());
-                } else {
-                    if (!cache.contains(path) || (cache.contains(path) && cache.getLastModified(path) < lastModified)) {                    
+                if (!cache.contains(path) || (cache.contains(path) && cache.getLastModified(path) < lastModified)) {
+                    if ("text/css".equals(mimeType)) {
+                        cache.setContent(path, (url != null ? engine.compile(url) : engine.compile(file)).getBytes(engine.getCharset()));
+                    } else {
                         cache.setContent(path, url != null ? LessUtils.readURL(url) : LessUtils.readFile(file)).setLastModified(path, lastModified);
                     }
-                    content = (byte[]) cache.getContent(path);
+                    if ("true".equals(getInitParameter("compress"))) {
+                        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+                        Reader in = new InputStreamReader(
+                                new ByteArrayInputStream(
+                                        (new String((byte[]) cache.getContent(path)))
+                                            .replaceFirst("/\\*", "/*!")
+                                            .getBytes(engine.getCharset())), 
+                                        engine.getCharset());
+                        Writer out = new OutputStreamWriter(baos, engine.getCharset());
+                        if ("text/css".equals(mimeType)) {
+                            CssCompressor compressor = new CssCompressor(in);
+                            in.close();
+                            compressor.compress(out, -1);
+                        } else if ("text/javascript".equals(mimeType)) {
+                            try {
+                                JavaScriptCompressor compressor = new JavaScriptCompressor(in, new ErrorReporter() {
+                                    public void warning(String message, String sourceName,
+                                            int line, String lineSource, int lineOffset) {
+                                        logger.warn(line < 0 ? message : line + ':' + lineOffset + ':' + message);
+                                    }
+                                    public void error(String message, String sourceName,
+                                            int line, String lineSource, int lineOffset) {
+                                        logger.warn(line < 0 ? message : line + ':' + lineOffset + ':' + message);
+                                    }
+                                    public EvaluatorException runtimeError(String message, String sourceName,
+                                            int line, String lineSource, int lineOffset) {
+                                        error(message, sourceName, line, lineSource, lineOffset);
+                                        return new EvaluatorException(message);
+                                    }
+                                });
+                                in.close();
+                                compressor.compress(out, -1, false, false, true, false);
+                            } catch (EvaluatorException e) {
+                                logger.error(e.getMessage());
+                            }
+                        }
+                        out.flush();
+                        cache.setContent(path, baos.toByteArray());
+                        out.close();
+                    }
                 }
+                content = (byte[]) cache.getContent(path);
+                
             } catch (LessException ce) {
                 logger.error("Error processing: " + request.getRequestURI());
                 throw new ServletException(ce.getMessage(), ce);
@@ -144,22 +195,17 @@ public class LessServlet extends HttpServlet {
             }
             
             if (gzip && "true".equals(getInitParameter("gzip"))) {
-                
                 response.setHeader("Content-Encoding", encoding);
                 ByteArrayOutputStream byteStream = new ByteArrayOutputStream();
                 GZIPOutputStream gz = new GZIPOutputStream(byteStream);
                 gz.write(content);
                 gz.finish();
-                
                 byte[] bytes = byteStream.toByteArray();
                 response.setContentLength(bytes.length);
                 response.getOutputStream().write(bytes);
-                
             } else {
-                
                 response.setContentLength(content.length);
                 response.getOutputStream().write(content);
-            
             }
 
             response.getOutputStream().flush();
